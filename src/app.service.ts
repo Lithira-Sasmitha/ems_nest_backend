6e4f12ba702e860437b14d846f56852b51ed1cd2
LITHIRA-SASMITHA <lithirasasmitha@gmail.com>
2025-11-22 18:23:27 +0530

    init connect db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectDataSource() private readonly dataSource: DataSource,
+  ) {}
+
+  // Database test
+  async checkDbConnection() {
+    try {
+      await this.dataSource.query('SELECT 1'); // test query
+      return {
+        status: 'connected',
+        message: 'MySQL Database is working!',
+      };
+    } catch (error: any) {
+      return {
+        status: 'error',
+        message: error?.message ?? 'Unknown database error',
+      };
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
