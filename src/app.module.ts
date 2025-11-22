4eab2115edda33eb35d1a3f61b2e14783d4310b0
LITHIRA-SASMITHA <lithirasasmitha@gmail.com>
2025-11-22 18:23:27 +0530

    init connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TestModule } from '../test/test.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST!,
+      port: Number(process.env.DB_PORT!),
+      username: process.env.DB_USER!,
+      password: process.env.DB_PASS!,
+      database: process.env.DB_NAME!,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+
+    TestModule,
+  ],
 })
 export class AppModule {}
